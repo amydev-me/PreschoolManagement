54248e77563747dcbae947c11ce1ab1f73b62e1c
Amy <moesat68@googlemail.com>
2018-04-18 09:21:32 +0630

    Grade ,Term

--- admin/views/payment/view.blade.php
+++ admin/views/payment/view.blade.php
@@ -3,7 +3,7 @@
 @section('payment','active')
 
 @section('style')
-    <link href=" https://printjs-4de6.kxcdn.com/print.min.css" rel="stylesheet">
+
 
 
     <style>
@@ -23,12 +23,17 @@
                 display: none;
             }
             .content{margin-left: 0px;}
-            section header, footer {
+            footer {
                 display: none;
             }
-
+            header {
+                display: none;
+            }
             @page {
                 margin: 0.5cm;
+            }
+            #not-print{
+                display:none;
             }
         }
 
@@ -37,143 +42,121 @@
 @section('content')
     <invoice-view inline-template>
         <div class="wraper container-fluid" v-cloak>
+            <div class="row"  id="not-print">
+                <div class="col-md-12">
+                    <div class="btn-toolbar" role="toolbar">
+                        <div class="pull-right">
+                            {{--<button type="button" class="btn btn-success m-r-5 m-b-10"><i class="fa fa-floppy-o"></i></button>--}}
+                            {{--<button type="button" class="btn btn-success m-r-5 m-b-10"><i class="fa fa-trash-o"></i></button>--}}
+                            <button  @click="print" class="btn btn-inverse m-r-5 m-b-10" ><i class="fa fa-print" ></i>Print</button>
 
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="btn-toolbar" role="toolbar">
+                            <button class="btn btn-purple" id="sa-basic" > <span>Send</span> <i class="fa fa-send m-l-10"></i> </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default" >
+                        <!-- <div class="panel-heading">
+                            <h4>Invoice</h4>
+                        </div> -->
+                        <div class="panel-body" id="section-to-print">
+                            <div class="clearfix">
+                                <div class="pull-left">
+                                    <h1 class="text-right"><img v-show="parentData.logo!='null'" class="thumb-md" :src="getImage()">@{{ parentData.title }}
+                                    </h1>
+                                </div>
                                 <div class="pull-right">
-                                    {{--<button type="button" class="btn btn-success m-r-5 m-b-10"><i class="fa fa-floppy-o"></i></button>--}}
-                                    {{--<button type="button" class="btn btn-success m-r-5 m-b-10"><i class="fa fa-trash-o"></i></button>--}}
-                                    <button  @click="print" class="btn btn-inverse m-r-5 m-b-10" ><i class="fa fa-print" ></i>Print</button>
+                                    <h4>Invoice # <br>
+                                        <strong>@{{ payment.invoice }}</strong>
+                                    </h4>
+                                </div>
+                            </div>
+                            <hr style="margin-top: 20px;">
+                            <div style="display: flex;flex-direction: row">
+                                    <div class="m-t-30"  style="width: 30%;margin-right: 30px !important;">
+                                        <h4 class="m-b-25">Invoice From</h4>
+                                        <address>
+                                            <strong>@{{ parentData.title }}</strong><br>
+                                            @{{ parentData.address }}<br>
+                                            <abbr title="Phone">P:</abbr>@{{ parentData.phone }}
+                                        </address>
 
-                                    <button class="btn btn-purple" id="sa-basic" > <span>Send</span> <i class="fa fa-send m-l-10"></i> </button>
+                                    </div>
+                                    <div class="m-t-30"  style="width: 30%">
+                                        <h4 class="m-b-25">Invoice To</h4>
+                                        <address>
+                                            <strong>@{{ student.fullName }}</strong><br>
+                                            @{{ student.address }}<br>
+                                            <abbr title="Phone">P:</abbr>@{{ student.phone }}
+                                        </address>
+
+                                    </div>
+                                    <div class="m-t-30" style="width: 40%;text-align: right;">
+                                        <p><strong>Invoice Date: </strong> @{{ formatDate(payment.payment_date) }}</p>
+                                        <p class="m-t-10"><strong>Status: </strong>
+                                            <span class="label label-success" style="background-color:#2eb398 " v-if="payment.status=='PAID'">@{{payment.status}}</span>
+                                            <span class="label label-danger"  style="background-color: #FF6C60" v-if="!(payment.due_date< currentdate) && payment.status=='UNPAID'">@{{payment.status}}</span>
+                                            <span class="label label-warning" style="background-color:#ebc142 " v-if="payment.due_date< currentdate &&payment.status=='UNPAID'">OVERDUE</span>
+
+                                        </p>
+                                        <p><strong>Due Date: </strong> @{{ formatDate(payment.due_date) }}</p>
+                                        {{--<p class="m-t-10"><strong>Order ID: </strong> #123456</p>--}}
+                                    </div>
+                            </div>
+                            <div class="m-h-20"></div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="table-responsive">
+                                        <table class="table m-t-30">
+                                            <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Description</th>
+                                                <th style="text-align: right">Amount</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+
+                                                <td>1</td>
+                                                <td>
+                                                    <span>@{{grade.academic.academicName +' '+term.termName}}</span><br>
+
+                                                    <span>@{{grade.gradeName+' ('+testFormat(term.start_date)+' - '+testFormat(term.end_date)+')'}}</span>
+                                                </td>
+
+                                                <td style="text-align: right">@{{ formatNumber(payment.amount) }}</td>
+                                            </tr>
+                                            <tr v-for="fee,index in fees">
+                                                <td>@{{index+2 }}</td>
+                                                <td>@{{ fee.feeName+ (fee.description?(' ('+ fee.description +') '):'')}}</td>
+                                                <td style="text-align: right">@{{formatNumber(fee.pivot.amount) }}</td>
+
+                                            </tr>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td style="text-align: right"></td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row" style="border-radius: 0px;">
+                                <div class="col-md-3 col-md-offset-9">
+                                    <h3 class="text-right" style="margin-right: 10px;"><span>$</span> @{{ formatNumber(payment.total) }}</h3>
+                                    {{--<hr style="text-align: right;width: 80%;">--}}
+                                    {{--<p class="text-right" style="margin-right: 10px;">Receipt Amount: @{{ formatNumber(payment.total-payment.balance) }}</p>--}}
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="panel panel-default" >
-                                <!-- <div class="panel-heading">
-                                    <h4>Invoice</h4>
-                                </div> -->
-                                <div class="panel-body" id="section-to-print">
-                                    <div class="clearfix">
-                                        <div class="pull-left">
-                                            <h1 class="text-right"><img v-show="parentData.logo!='null'" class="thumb-md" :src="getImage()">@{{ parentData.title }}
-                                            </h1>
-                                        </div>
-                                        <div class="pull-right">
-                                            <h4>Invoice # <br>
-                                                <strong>@{{ payment.invoice }}</strong>
-                                            </h4>
-                                        </div>
-                                    </div>
-                                    <hr style="margin-top: 20px;">
-                                    <div style="display: flex;flex-direction: row">
-
-
-                                            <div class="m-t-30"  style="width: 30%;margin-right: 30px !important;">
-                                                <h4 class="m-b-25">Invoice From</h4>
-                                                <address>
-                                                    <strong>@{{ parentData.title }}</strong><br>
-                                                    @{{ parentData.address }}<br>
-                                                    <abbr title="Phone">P:</abbr>@{{ parentData.phone }}
-                                                </address>
-
-                                            </div>
-                                            <div class="m-t-30"  style="width: 30%">
-                                                <h4 class="m-b-25">Invoice To</h4>
-                                                <address>
-                                                    <strong>@{{ student.fullName }}</strong><br>
-                                                    @{{ student.address }}<br>
-                                                    <abbr title="Phone">P:</abbr>@{{ student.phone }}
-                                                </address>
-
-                                            </div>
-                                            <div class="m-t-30" style="width: 40%;text-align: right;">
-                                                <p><strong>Date: </strong> @{{ formatDate(payment.payment_date) }}</p>
-                                                <p class="m-t-10"><strong>Status: </strong>
-                                                    <span class="label label-success" v-if="payment.status=='PAID'">@{{payment.status}}</span>
-                                                    <span class="label label-danger"  v-if="!(payment.due_date< currentdate) && payment.status=='UNPAID'">@{{payment.status}}</span>
-                                                    <span class="label label-warning" v-if="payment.due_date< currentdate &&payment.status=='UNPAID'">OVERDUE</span>
-
-                                                </p>
-                                                {{--<p class="m-t-10"><strong>Order ID: </strong> #123456</p>--}}
-                                            </div>
-
-                                    </div>
-                                    <div class="m-h-50"></div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="table-responsive">
-                                                <table class="table m-t-30">
-                                                    <thead>
-                                                    <tr>
-                                                        <th>#</th>
-                                                        <th>Description</th>
-                                                        <th style="text-align: right">Amount</th>
-                                                    </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                    <tr>
-
-                                                        <td>1</td>
-                                                        <td>
-                                                            <span>@{{grade.academic.academicName +' '+term.termName}}</span><br>
-
-                                                            <span>@{{grade.gradeName+' ('+testFormat(term.start_date)+' - '+testFormat(term.end_date)+')'}}</span>
-                                                        </td>
-
-                                                        <td style="text-align: right">@{{ formatNumber(payment.amount) }}</td>
-                                                    </tr>
-                                                    <tr v-for="fee,index in fees">
-                                                        <td>@{{index+2 }}</td>
-                                                        <td>@{{ fee.feeName+ (fee.description?(' ('+ fee.description +') '):'')}}</td>
-                                                        <td style="text-align: right">@{{formatNumber(fee.pivot.amount) }}</td>
-
-                                                    </tr>
-                                                    <tr>
-
-
-                                                        <td></td>
-                                                        <td></td>
-
-                                                        <td style="text-align: right"></td>
-
-                                                    </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row" style="border-radius: 0px;">
-                                        <div class="col-md-3 col-md-offset-9">
-                                            <h3 class="text-right" style="margin-right: 10px;"><span>$</span> @{{ formatNumber(payment.total) }}</h3>
-                                            <hr style="width: 50%;text-align: right;">
-
-                                            {{--<p class="text-right" style="margin-right: 10px;">Receipt Amount: @{{ formatNumber(payment.total-payment.balance) }}</p>--}}
-
-
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
+                </div>
+            </div>
         </div>
     </invoice-view>
 
 @endsection
-@section('script')
-    {{--<script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>--}}
-    @endsection
-{{--<div class="hidden-print">--}}
-    {{--<div class="pull-right">--}}
-        {{--<button @click="print" class="btn btn-inverse" style="margin-right: 30px;"><i class="fa fa-print"></i> Print</button>--}}
-    {{--</div>--}}
-{{--</div>--}}
