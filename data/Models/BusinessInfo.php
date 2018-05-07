3dea10ae1cd8c08a5acf2c46c6f70fc046e01153
Amy <moesat68@googlemail.com>
2018-05-07 09:44:23 +0630

    Send Mail,Setting

--- data/Models/BusinessInfo.php
+++ data/Models/BusinessInfo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusinessInfo extends Model
 {
-    protected $fillable=['title','business_type','phone','address','email','email_subject','email_password','email_text','website','facebook','fax','footer','note','logo','invoice_logo','login_text','academic_id','instruction'];
+    protected $fillable=['title','business_type','phone','address','email',
+        'email_host',
+        'email_port',
+        'email_encryption',
+
+        'email_subject','email_password','email_text','website','facebook','fax','footer','note','logo','invoice_logo','login_text','academic_id','instruction'];
 }
